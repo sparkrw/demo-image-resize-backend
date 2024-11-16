4939f354086dc494fd64b8d377bacb003c62b4a4
spark <innocentchris@gmail.com>
2024-11-17 04:11:34 +0900

    cmt

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# Stop any running server managed by forever
-if forever list | grep -q "server.js"; then
-  forever stop src/server.js
-  echo "Stopped running server."
-else
-  echo "No running server to stop."
-fi
+forever stopall

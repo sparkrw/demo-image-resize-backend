b99ef338f390d8a371137ac8918f6cd472d40fc5
spark <innocentchris@gmail.com>
2024-11-17 11:38:09 +0900

    initial

--- src/server.js
+++ src/server.js
@@ -57,7 +57,7 @@ app.post('/resize', async (req, res) => {
         console.log('Sending response with resized image');
         res.json({
             resizedImageBase64: resizedBase64,
-            comment: "Hello, AWS Classroom"
+            comment: "Hello, This IS AWS Classroom"
         });
     } catch (error) {
         console.error('Error processing image:', error);
